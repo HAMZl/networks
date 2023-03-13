4f59fb183bf2f00c42d10e29c12162d735f1430f
Hamza Ahmed <sc21h2a@leeds.ac.uk>
2023-03-13 22:41:03 +0000

    Established client server connection and created log file to keep track of all requests made to server #2.

--- cwk/server/Server.java
+++ cwk/server/Server.java
@@ -1,7 +1,7 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
-
+import java.time.*;
 public class Server 
 {
 	public void runServer() {
@@ -11,18 +11,21 @@ public class Server
         {
             // we use port 5555.
             ServerSocket serverSock = new ServerSocket(6001);
+			file.createNewFile();
 
             while( true )
             {
                 // Accept; blocking; will not return until a client has made contact.
                 Socket sock = serverSock.accept();
 
-                // Get information about the connection and the date/time, and print to screen. Normally you would not print
-                // this information - it would e.g. be sent to a log file - this is just for demonstration purposes.
+                // writing to log file
                 InetAddress inet = sock.getInetAddress();
-                Date date = new Date();
-                System.out.println("\nDate " + date.toString() );
-                System.out.println("Connection made from " + inet.getHostName());
+                LocalDate localDate = LocalDate.now(); // current date
+				LocalTime localTime = LocalTime.now(); // current time
+				FileWriter fileWriter = new FileWriter("log.txt", true); // open file to append
+				String content = localDate + "|" + localTime + "|" + inet.getHostAddress() + "\n";
+				fileWriter.write(content); // write to file
+				fileWriter.close(); // close file
 
                 // Send a single line of text to the client.
                 PrintWriter writer = new PrintWriter(sock.getOutputStream());
