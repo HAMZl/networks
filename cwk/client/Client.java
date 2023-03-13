e7326d3067a158ff7ad6f663e33b4ce3866ff1fc
Hamza Ahmed <sc21h2a@leeds.ac.uk>
2023-03-13 21:46:10 +0000

    Initialised files for coursework #1

--- cwk/client/Client.java
+++ cwk/client/Client.java
@@ -1,6 +1,29 @@
+import java.io.*;
+import java.net.*;
+
 public class Client 
 {
-	public static void main( String[] args )
-	{
+	public void connect() {
+
+        try {
+
+            // Try and create the socket. The server is assumed to be running on the same host ('localhost'),
+            // so first run 'KnockKnockServer' in another shell.
+            Socket s = new Socket( "localhost", 6001 );
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream() ) );
+            String advice = reader.readLine();
+            System.out.println(advice);
+            reader.close();
+            s.close();
+        }
+        catch( IOException e )
+        {
+            System.out.println( e );
+        }
+    }
+	public static void main( String[] args ){
+		Client client = new Client();
+        client.connect();
 	}
 }
--- cwk/server/Server.java
+++ cwk/server/Server.java
@@ -1,6 +1,43 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
 public class Server 
 {
+	public void runServer() {
+		File file = new File("log.txt");
+		String[] clientArguments = null;
+        try
+        {
+            // we use port 5555.
+            ServerSocket serverSock = new ServerSocket(6001);
+
+            while( true )
+            {
+                // Accept; blocking; will not return until a client has made contact.
+                Socket sock = serverSock.accept();
+
+                // Get information about the connection and the date/time, and print to screen. Normally you would not print
+                // this information - it would e.g. be sent to a log file - this is just for demonstration purposes.
+                InetAddress inet = sock.getInetAddress();
+                Date date = new Date();
+                System.out.println("\nDate " + date.toString() );
+                System.out.println("Connection made from " + inet.getHostName());
+
+                // Send a single line of text to the client.
+                PrintWriter writer = new PrintWriter(sock.getOutputStream());
+                writer.println("Connection has been made.");    		 // Write to client
+                writer.close();
+                sock.close();
+            }
+        }
+        catch (IOException ex) {
+            System.out.println( ex );
+        }
+    }
 	public static void main( String[] args )
 	{
+		Server server = new Server();
+        server.runServer();
 	}
 }
